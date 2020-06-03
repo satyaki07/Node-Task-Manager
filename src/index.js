29a83d25c4d3324e050579930e253c489c864ef1
Satyaki Bose <satyakibose98@gmail.com>
2020-06-03 11:11:42 +0530

    Final check

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-const express = require('express');
-require('./db/mongoose.js');
+const app = require('./app')
 
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-
-
-
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
-})
+});
 
